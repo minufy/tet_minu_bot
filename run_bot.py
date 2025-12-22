fa82f9d70d37eed73da713a72275655b014c5df5
minu <alsdn638@gmail.com>
2025-12-22 14:29:01 +0900

    edit started

--- run_bot.py
+++ run_bot.py
@@ -1,5 +1,6 @@
 import pygame
 import zmq
+import sys
 from bot import Bot
 from board import TestBoard
 from tet_utils.minos import Mino
@@ -17,22 +18,43 @@ class GameEmu:
         self.queue = game_state["queue"]
         self.mino = Mino(game_state["mino_type"], 3, self.board.h//2)
 
+index = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+print(f"starting bot {index}")
+
 context = zmq.Context()
-print("connecting..")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
-socket.send_json({"events": []})
+socket.send_json({
+    "events": [],
+    "index": index
+})
 
-game = GameEmu()
-game_state = socket.recv_json()
-game.update(game_state)
-bot = Bot(game, 1)
+game = None
+bot = None
+
+started = False
 
 while True:
     dt = clock.tick(30)
-    events = bot.get_events()
-    socket.send_json({"events": events})
-     
+
     game_state = socket.recv_json()
-    game.update(game_state)
-    bot.update(dt)
+    if game_state["state"] == "started" and not started:
+        game = GameEmu()
+        game.update(game_state)
+        bot = Bot(game, 200+100*index)
+        started = True
+
+    if started:    
+        events = bot.get_events()
+        socket.send_json({
+            "events": events,
+            "index": index
+        })
+        
+        game.update(game_state)
+        bot.update(dt)
+    else:
+        socket.send_json({
+            "events": [],
+            "index": index
+        })
